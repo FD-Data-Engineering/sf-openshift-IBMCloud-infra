185bf16b211a93128d05aaa7e81447d7453a9c4f
pkwame <45004664+pkwame@users.noreply.github.com>
2023-01-09 16:06:29 +0000

    refactor

--- terraform/local_tf_deploy.sh
+++ terraform/local_tf_deploy.sh
@@ -4,21 +4,23 @@ set -ex
 
 cd docker-worker
 
+echo "[INFO] Building Openshift worker Image"
+
 PARENT_DIR=$(basename "${PWD%/*}")
 CURRENT_DIR="${PWD##*/}"
 IMAGE_NAME="$PARENT_DIR/$CURRENT_DIR"
 TAG="latest"
 
-docker build -t ${REGISTRY}${IMAGE_NAME}:${TAG} .
+docker build -t ${IMAGE_NAME}:${TAG} .
 
 cd ../105-ibm-vpc-openshift
 
 echo "[INFO] Terraform Init - Initialise the Terraform execution using remote backend"
 
-docker run --rm -it -v /$PWD://data -w //data ${REGISTRY}${IMAGE_NAME}:${TAG} init
+docker run --rm -it -v /$PWD://data -w //data ${IMAGE_NAME}:${TAG} init
 
 echo "[INFO] Terraform Plan - Create the Terraform execution plan in the remote backend"
-docker run --rm -it -v /$PWD://data -w //data ${REGISTRY}${IMAGE_NAME}:${TAG} plan
+docker run --rm -it -v /$PWD://data -w //data ${IMAGE_NAME}:${TAG} plan
 
 echo "[INFO] Terraform Apply - Apply the Terraform changes"
-docker run --rm -it -v /$PWD://data -w //data ${REGISTRY}${IMAGE_NAME}:${TAG} apply -auto-approve
+docker run --rm -it -v /$PWD://data -w //data ${IMAGE_NAME}:${TAG} apply -auto-approve
